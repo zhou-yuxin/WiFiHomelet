209a41bfbea50c588b36e5f1a96c691dfeae63fc
zhou-yuxin <504849766@qq.com>
2024-10-09 16:17:44 +0800

    update

--- WiFiHomelet.h
+++ WiFiHomelet.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Ticker.h>
 #include <Updater.h>
 #include <LittleFS.h>
 #include <ArduinoJson.h>
@@ -9,88 +10,18 @@
 #include <ESP8266WebServer.h>
 #include <AsyncHTTPRequest_Generic.h>
 
-#include <queue>
-
 class WiFiHomelet {
 
 public:
     using WebServer = ESP8266WebServer;
 
 private:
-    static constexpr int NREQUESTS = 3;
-
     WiFiEventHandler handlers[3];
     WebServer web;
     File file;
     WiFiUDP udp;
     mDNSResolver::Resolver resolver;
-    std::queue<String> urls;
-
-    struct Epilogue {
-        std::function<void(void)> func;
-        uint32_t start;
-        uint32_t delay;
-
-        void set(std::function<void(void)> func, uint32_t delay) {
-            this->func = func;
-            this->delay = delay;
-            start = millis();
-        }
-
-        void operator()() {
-            if(func && millis() - start >= delay) {
-                func();
-                func = nullptr;
-            }
-        }
-    }
-    epilogue;
-
-    struct Request {
-        static constexpr uint32_t COOLING_TIME = 3000;
-
-        AsyncHTTPRequest request;
-        uint32_t start;
-
-        Request() {
-            request.onReadyStateChange([](void* self, AsyncHTTPRequest* request, int state) {
-#ifdef DEBUG
-                Serial.printf("HTTP state: %d\n", state);
-#endif
-                if(state == readyStateOpened) {
-                    request->send();
-                }
-                else if(state == readyStateDone) {
-#ifdef DEBUG
-                    Serial.printf("HTTP code: %d, text: %s\n",
-                        request->responseHTTPcode(), request->responseText().c_str());
-#endif
-                    reinterpret_cast<Request*>(self)->start = std::max(2UL, millis());
-                }
-            },
-            this);
-            start = 0;
-        }
-
-        void tick() {
-            if(start > 1 && millis() - start >= COOLING_TIME) {
-                start = 0;
-            }
-        }
-
-        bool get(const char* url) {
-            if(start != 0) {
-                return false;
-            }
-            start = 1;
-            request.open("GET", url);
-#ifdef DEBUG
-            Serial.printf("request to %s\n", url);
-#endif
-            return true;
-        }
-    }
-    requests[NREQUESTS];
+    Ticker ticker;
 
 public:
     WiFiHomelet(): resolver(udp) {
@@ -141,7 +72,7 @@ public:
             json["code"] = 0;
             json["ssid"] = ssid;
             json["passphrase"] = passphrase;
-            epilogue.set([=](){ WiFi.begin(ssid, passphrase); }, 3000);
+            ticker.once(3, [=](){ WiFi.begin(ssid, passphrase); });
             return true;
         });
         onRequest("/info", [&](WebServer&, JsonDocument& json) -> bool {
@@ -168,25 +99,22 @@ public:
             return true;
         }, 512);
         onRequest("/restart", [&](WebServer&, JsonDocument& json) -> bool {
-            epilogue.set([](){ ESP.restart(); }, 5000);
             json["code"] = 0;
+            ticker.once(3, [](){ ESP.restart(); });
             return true;
         });
         onRequest("/delete", [](WebServer& web, JsonDocument& json) -> bool {
             if(!web.hasArg("file")) {
                 json["code"] = -1;
-                json["error"] = "<file> = * / [file name] is mandatory";
+                json["error"] = "<file> = */[file name] is mandatory";
                 return true;
             }
             const String& file = web.arg("file");
             if(file.equals("*")) {
-                Dir dir = LittleFS.openDir("/");
-                while(dir.next()) {
-                    if(!LittleFS.remove(dir.fileName())) {
-                        json["code"] = -1;
-                        json["error"] = "failed to delete " + dir.fileName();
-                        return true;
-                    }
+                if(!LittleFS.format()) {
+                    json["code"] = -1;
+                    json["error"] = "failed to format filesystem";
+                    return true;
                 }
             }
             else if(!LittleFS.remove(file)) {
@@ -229,6 +157,9 @@ public:
                         code = -1;
                         msg = "failed to write to " + upload.filename;
                     }
+#ifdef DEBUG
+                    Serial.printf("so far uploaded %lu bytes\n", upload.totalSize);
+#endif
                 }
                 else if(upload.status == UPLOAD_FILE_END) {
                     code = 0;
@@ -239,9 +170,6 @@ public:
                     code = -1;
                     msg = "aborted to upload";
                 }
-#ifdef DEBUG
-                Serial.printf("so far uploaded %lu bytes\n", upload.totalSize);
-#endif
                 if(code == 0) {
                     web.send(200, "application/json", "{code: 0, size: " + msg + "}");
                 }
@@ -275,6 +203,9 @@ public:
                         code = -1;
                         msg = "failed to write to flash";
                     }
+#ifdef DEBUG
+                    Serial.printf("so far uploaded %lu bytes\n", upload.totalSize);
+#endif
                 }
                 else if(upload.status == UPLOAD_FILE_END) {
                     if(Update.end(true)) {
@@ -291,9 +222,6 @@ public:
                     code = -1;
                     msg = "aborted to upload";
                 }
-#ifdef DEBUG
-                Serial.printf("so far uploaded %lu bytes\n", upload.totalSize);
-#endif
                 if(code == 0) {
                     web.send(200, "application/json", "{code: 0, size: " + msg + "}");
                 }
@@ -319,7 +247,7 @@ public:
         });
     }
 
-    bool request(const char* URL) {
+    bool httpGet(const char* URL, std::function<void(const String&)> callback = nullptr) {
         String url(URL);
         if(!url.startsWith("http://")) {
             return false;
@@ -329,15 +257,38 @@ public:
         String host = end < 0 ? url.substring(start) : url.substring(start, end);
         if(host.endsWith(".local")) {
             IPAddress ip = resolver.search(host.c_str());
-#ifdef DEBUG
-            Serial.printf("mDNS: %s => %s\n", host.c_str(), ip.toString().c_str());
-#endif
             if(ip == INADDR_NONE) {
                 return false;
             }
+#ifdef DEBUG
+            Serial.printf("mDNS: %s => %s\n", host.c_str(), ip.toString().c_str());
+#endif
             url = "http://" + ip.toString() + (end < 0 ? emptyString : url.substring(end));
         }
-        urls.push(url);
+#ifdef DEBUG
+        Serial.printf("request to %s\n", url.c_str());
+#endif
+        auto* request = new AsyncHTTPRequest;
+        request->onReadyStateChange(
+            [=](void*, AsyncHTTPRequest* request, int state) {
+#ifdef DEBUG
+                Serial.printf("HTTP state: %d\n", state);
+#endif
+                if(state == readyStateOpened) {
+                    request->send();
+                }
+                else if(state == readyStateDone) {
+#ifdef DEBUG
+                    Serial.printf("HTTP code: %d, text: %s\n",
+                        request->responseHTTPcode(), request->responseText().c_str());
+#endif
+                    if(callback) {
+                        callback(request->responseText());
+                    }
+                    delete request;
+                }
+            });
+        request->open("GET", url.c_str());
         return true;
     }
 
@@ -345,19 +296,6 @@ public:
         MDNS.update();
         web.handleClient();
         resolver.loop();
-        epilogue();
-        for(int i = 0; i < NREQUESTS; i++) {
-            requests[i].tick();
-        }
-        if(!urls.empty()) {
-            const char* url = urls.front().c_str();
-            for(int i = 0; i < NREQUESTS; i++) {
-                if(requests[i].get(url)) {
-                    urls.pop();
-                    break;
-                }
-            }
-        }
     }
 
 };
